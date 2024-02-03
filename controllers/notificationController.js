07463795a92207ec52318737eb8f450d8e670129
Alvaro Mendoza <alvaromendozap99@gmail.com>
2024-02-03 16:34:41 -0500

    Added filter to notification controller
    Added filter to notification controller

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -6,25 +6,56 @@ const retrieveNotifications = async (req, res) => {
     const id = new mongoose.Types.ObjectId(req.user.sub);
     const notifications = await Notifications.aggregate([
         {
-            $lookup: {from: 'users', localField: 'user', foreignField: '_id', as: 'User'}
-        },{
-            $unwind: '$User'
-        },{
-            $lookup: {from: 'posts', localField: 'postId', foreignField: '_id', as: 'Post'}
-        },{
-            $unwind: '$Post'
-        },{
-            $match: {'Post.user':id}
-        },{
-            $sort: {'date':-1}
-        },{
-            $project: {'user':0, 'postId':0, 'User.password':0, 'User.role':0}
+            $lookup: { from: 'users', localField: 'user', foreignField: '_id', as: 'userInfo' }
+        }, {
+            $unwind: '$userInfo'
+        }, {
+            $lookup: { from: 'posts', localField: 'postId', foreignField: '_id', as: 'postInfo' }
+        }, {
+            $unwind: '$postInfo'
+        }, {
+            $match: { 'postInfo.user': id }
+        }, {
+            $sort: { 'date': -1 }
+        }, {
+            $project: { 'user': 0, 'postId': 0, 'userInfo.password': 0, 'userInfo.role': 0 }
         }
     ])
 
-    return res.status(200).json({notifications:notifications})
+    return res.status(200).json({ notifications: notifications })
 };
 
+const retrieveNotificationByCategory = async (req, res) => {
+    const id = new mongoose.Types.ObjectId(req.user.sub);
+    const param = req.params.category;
+    const category = param.substr(0, 1).toUpperCase() + param.substr(1, param.length - 1);
+
+    const notifications = await Notifications.aggregate([
+        {
+            $lookup: { from: 'users', localField: 'user', foreignField: '_id', as: 'userInfo' }
+        }, {
+            $unwind: '$userInfo'
+        }, {
+            $lookup: { from: 'posts', localField: 'postId', foreignField: '_id', as: 'postInfo' }
+        }, {
+            $unwind: '$postInfo'
+        }, {
+            $match: { 'postInfo.user': id, 'typeNotification': category }
+        }, {
+            $project: {
+                'user': 0,
+                'postId': 0,
+                'userInfo.password': 0,
+                'userInfo.role': 0
+            }
+        }
+    ]);
+    return res.status(200).json({
+        notifications
+    });
+}
+
 module.exports = {
-    retrieveNotifications
+    retrieveNotifications,
+    retrieveNotificationByCategory
 }
--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -6,4 +6,7 @@ const router = express.Router();
 router.route('/')
     .get(verifyJWT, notificationController.retrieveNotifications);
 
+router.route('/:category')
+    .get(verifyJWT, notificationController.retrieveNotificationByCategory);
+
 module.exports = router;
